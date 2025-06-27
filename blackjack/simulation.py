1d1c5f6d4cf8c160001b0ad6154444956c248ab3
Anthony Quigley <aquigley1777@gmail.com>
2025-06-26 23:14:43 -0700

    Remove simulation class

--- blackjack/simulation.py
+++
@@ -1,7 +0,0 @@
-class Simulation:
-    def __init__(self, num_games: int) -> None:
-        self.num_games: int = num_games
-        self.results: list[dict[str, object]] = []
-
-    def run(self) -> None:
-        pass
